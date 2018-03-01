c7ad42954d9994cbd1a1d7fa1567f7ce1cd7657b
Kevin Robinson <kevin.robinson@outlook.com>
2018-03-01 09:46:12 +0000

    changed component name to camel case

--- Core/templateCopier.js
+++ Core/templateCopier.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const settings = require('../settings');
-const FileHound = require('filehound');
+const _ = require('lodash');
 
 var fs = require('fs');
 var copyTplsWithData = function (runner, source, dest, feature) {
@@ -19,65 +19,34 @@ var copyTplsWithData = function (runner, source, dest, feature) {
   }
 
 
+  let processTemplate = function(templateFileName){
+    if (templateFileName.includes('.')) {
+      var currentFile = runner.templatePath(srcPath + templateFileName);
+      var destPath = dest + templateFileName;
+
+      
+      var data = {
+          appName: 'Test App',
+          featureName: feature,
+          featureNameCC: _.camelCase(feature),
+      };
+      console.log('data1=' + JSON.stringify(data));
+
+      runner.fs.copyTpl(currentFile, runner.destinationPath(destPath), {
+        data: data
+      });
+    }
+  }
+
+
   fs.readdir(runner.templatePath(srcPath), function (err, items) {
 
     for (var f in items) {
-      if (items[f].includes('.')) {
-
-
-        var currentFile = runner.templatePath(srcPath + items[f]);
-        var destPath = dest + items[f].replace('_', '');
-
-        
-        var data = {
-            appName: 'Test App',
-            featureName: feature
-        };
-        console.log('data1=' + JSON.stringify(data));
-
-        runner.fs.copyTpl(currentFile, runner.destinationPath(destPath), {
-          data: data
-        });
-      }
+      processTemplate(items[f]);     
     }
   })
+
+ 
 }
-var copyTplsWithDataFeature = function (runner, source, dest, feature) {
-  
-    console.log('source = ' + source);
-    console.log('path = ' + runner.templatePath(source));
-    console.log('featureName = ' + feature);
-  
-  
-    var srcPath = '';
-  
-    if (source == '') {
-      srcPath = runner.templatePath() + '/';
-    } else {
-      srcPath = runner.templatePath(source);
-    }
-  
-  
-    fs.readdir(runner.templatePath(srcPath), function (err, items) {
-  
-      for (var f in items) {
-        if (items[f].includes('.')) {
-    
-          var currentFile = runner.templatePath(srcPath + items[f]);
-          var destPath = dest + items[f].replace('_', '');
-  
-          
-          var data = {
-              appName: 'Test App',
-              featureName: feature
-          };
-          console.log('data1=' + JSON.stringify(data));
-  
-          runner.fs.copyTpl(currentFile, runner.destinationPath(destPath), {
-            data: data
-          });
-        }
-      }
-    })
-  }
+
 module.exports.copyTplsWithData = copyTplsWithData;
