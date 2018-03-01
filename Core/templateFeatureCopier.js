cfccefb35aa1a52a8774b1b595885504a1697a57
Kevin Robinson <kevin.robinson@outlook.com>
2018-03-01 13:21:05 +0000

    Home and Details Working

--- Core/templateFeatureCopier.js
+++ Core/templateFeatureCopier.js
@@ -9,7 +9,7 @@ let Process = function (data, currentsrcPath, currentDestPath, runner, feature) 
 
   for (var f in files) {
     let src = currentsrcPath + files[f];    
-    let des = 'C:/GeneratorsOutput/Template/src/app/' + feature + '/' + files[f];
+    let des = currentDestPath + files[f];
 
     runner.fs.copyTpl(src, des,  {data: data    });  
   }
@@ -21,17 +21,20 @@ let processFeatureTemplate = function (feature, currentDestPath, items) {
 
 }
 
-let copyTplsWithDataFeature = function (runner, source, dest, data, features) {
+let copyTplsWithDataFeature = function (runner, source, dest, data, features, sub) {
  
 
   for (var feature in features) {
     data.featureName = feature,
     data.featureNameCC = _.camelCase(feature)
-    var destpath = dest + '/' + feature + '/details/';
+    var destpath = dest + '/' + feature + '/' + sub + '/';
+
+    console.log('destpath = ' + destpath);
 
     Process(data, runner.templatePath(source), destpath, runner, feature);
   }
 
 }
+
 
 module.exports.copyTplsWithDataFeature = copyTplsWithDataFeature;
