897c9ccc278f2240ad49aee7cdf77bd4b090fe0d
Kevin Robinson <kevin.robinson@outlook.com>
2018-03-01 10:02:35 +0000

    changed to data

--- Core/templateCopier.js
+++ Core/templateCopier.js
@@ -1,16 +1,29 @@
 const chalk = require('chalk');
 const settings = require('../settings');
 const _ = require('lodash');
-
 var fs = require('fs');
-var copyTplsWithData = function (runner, source, dest, feature) {
-
-  console.log('source = ' + source);
-  console.log('path = ' + runner.templatePath(source));
-  console.log('featureName = ' + feature);
 
 
-  var srcPath = '';
+
+var copyTplsWithData = function (runner, source, dest, data) {
+
+  let processTemplate = function (templateFileName) {
+
+      
+      if (templateFileName.includes('.')) {
+    
+        var currentFile = runner.templatePath(srcPath + templateFileName);
+        var destPath = dest + templateFileName;
+        
+     
+    
+        runner.fs.copyTpl(currentFile, runner.destinationPath(destPath), {
+          data: data
+        });
+      }
+    }
+
+   var srcPath = '';
 
   if (source == '') {
     srcPath = runner.templatePath() + '/';
@@ -18,35 +31,11 @@ var copyTplsWithData = function (runner, source, dest, feature) {
     srcPath = runner.templatePath(source);
   }
 
-
-  let processTemplate = function(templateFileName){
-    if (templateFileName.includes('.')) {
-      var currentFile = runner.templatePath(srcPath + templateFileName);
-      var destPath = dest + templateFileName;
-
-      
-      var data = {
-          appName: 'Test App',
-          featureName: feature,
-          featureNameCC: _.camelCase(feature),
-      };
-      console.log('data1=' + JSON.stringify(data));
-
-      runner.fs.copyTpl(currentFile, runner.destinationPath(destPath), {
-        data: data
-      });
-    }
-  }
-
-
   fs.readdir(runner.templatePath(srcPath), function (err, items) {
-
     for (var f in items) {
-      processTemplate(items[f]);     
+      processTemplate(items[f]);
     }
   })
-
- 
 }
 
 module.exports.copyTplsWithData = copyTplsWithData;
