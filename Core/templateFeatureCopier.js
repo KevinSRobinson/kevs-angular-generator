39a589f28843e206653178ee1d8dcd5ed3dd004f
Kevin Robinson <kevin.robinson@outlook.com>
2018-03-01 12:28:24 +0000

    added import components

--- Core/templateFeatureCopier.js
+++ Core/templateFeatureCopier.js
@@ -11,15 +11,8 @@ let Process = function (data, currentsrcPath, currentDestPath, runner, feature) 
     let src = currentsrcPath + files[f];    
     let des = 'C:/GeneratorsOutput/Template/src/app/' + feature + '/' + files[f];
 
-    //let currentFile = runner.templatePath(currentsrcPath + templateFileName);
-
-    console.log('src=' + src);
-    console.log('des=' + des);
-
-    runner.fs.copyTpl(src, des,  {data: data    }); 
- 
+    runner.fs.copyTpl(src, des,  {data: data    });  
   }
-
 }
 
 let processFeatureTemplate = function (feature, currentDestPath, items) {
@@ -30,23 +23,14 @@ let processFeatureTemplate = function (feature, currentDestPath, items) {
 
 let copyTplsWithDataFeature = function (runner, source, dest, data, features) {
  
- 
 
   for (var feature in features) {
-    console.log('feature = ' + feature);
-    console.log('-------------------------------------------------------------------------------');
     data.featureName = feature,
     data.featureNameCC = _.camelCase(feature)
     var destpath = dest + '/' + feature + '/details/';
 
-
     Process(data, runner.templatePath(source), destpath, runner, feature);
-    console.log('------------------------------------------------------');
   }
-
-
-
-
 
 }
 
