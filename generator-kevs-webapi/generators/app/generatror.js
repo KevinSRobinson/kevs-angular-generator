3f5ff41bfd4ecbeccda4bd034aa34c758fa0d1a5
Kevin Robinson <kevin.robinson@outlook.com>
2018-03-09 08:05:43 +0000

    e

---
+++ generator-kevs-webapi/generators/app/generatror.js
@@ -0,0 +1,37 @@
+const copier = require('../../../Core/templateCopier');
+const features = require('../../../features.json');
+const templateFeatureCopier = require('../../../Core/templateFeatureCopier');
+
+
+
+const _ = require('lodash');
+
+let generate = function (runner) {
+
+    var dest = 'C:/GeneratorsOutput/Webapi/';
+
+    var data = {
+      appName: 'Test App',
+      featureName: '',
+    };
+
+    //  copier.copyTplsWithData(runner,  '', dest, '');
+    //  copier.copyTplsWithData(runner,  'src/', dest + '/src/', '');
+    //  copier.copyTplsWithData(runner,  'src/WebApplication2/', dest + '/src/WebApplication2/', '');
+
+
+    templateFeatureCopier.copyTplsWithDataFeature1(runner,  'src/WebApplication2/Controllers/', dest + '/src/WebApplication2/Controllers/', data, features);
+
+
+     
+     
+     //templateFeatureCopier.copyTplsWithDataFeature(runner,  'src/WebApplication2/Controllers/',  dest + 'src/WebApplication2/Controllers/', data, features, '');
+
+
+
+    //  copier.copyTplsWithData(runner,  'src/WebApplication2/Properties/', dest + '/src/WebApplication2/Properties/', '');
+    //  copier.copyTplsWithData(runner,  'src/WebApplication2/wwwroot/', dest + '/src/WebApplication2/wwwroot/', '');
+     
+}
+
+module.exports.generate = generate;
