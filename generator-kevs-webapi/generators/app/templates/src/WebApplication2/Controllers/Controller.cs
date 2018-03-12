a307d13cada9b484af2981f281741d72e7d063cf
Kevin Robinson <kevin.robinson@outlook.com>
2018-03-12 13:16:00 +0000

    Added Load By Id to Apis

--- generator-kevs-webapi/generators/app/templates/src/WebApplication2/Controllers/Controller.cs
+++ generator-kevs-webapi/generators/app/templates/src/WebApplication2/Controllers/Controller.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Cors;
-
+using System.Linq;
 namespace WebApplication2.Controllers
 {
 
@@ -9,20 +9,24 @@ namespace WebApplication2.Controllers
     [Route("api/[controller]")]
     public class <%=data.featureName%>Controller : Controller
     {
+        private List<<%=data.featureName%>Dto> _<%=data.featureNameCC%>  = new List<<%=data.featureName%>Dto>();
 
-
+        public <%=data.featureName%>Controller()
+        {
+            _<%=data.featureNameCC%> = <%- data.getFakeData(data.featureName) %>
+        }
        
 
         [HttpGet]
         public IEnumerable<<%=data.featureName%>Dto> Get()
         {
-            <%- data.getFakeData(data.featureName) %>
+           return _<%=data.featureNameCC%>; %>
         }
         
         [HttpGet("{id}")]
-        public string Get(int id)
+        public <%=data.featureName%>Dto Get(int id)
         {
-            return "<%=data.featureName%>" + id.ToString();
+            return _<%=data.featureNameCC%>.FirstOrDefault(x => x.Id == id);
         }
     }
 
